94a29461366cb9455c66298ba4f67e08c49b4f29
Arturo <arturo.ortueta@gmail.com>
2019-04-18 16:45:42 -0500

    Se añadieron las funcionalidades de los botones: multiplicación, division y resta.

--- app/src/main/java/com/cupsoft/laboratorio1/MainActivity.java
+++ app/src/main/java/com/cupsoft/laboratorio1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cupsoft.laboratorio1;
 
+import android.net.sip.SipSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
     EditText txt1, txt2;
-    Button btncalcular;
+    Button btncalcular, btnrestar, btnmultiplicar, btndividir;
     TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +20,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txt1 = findViewById(R.id.txt1);
         txt2 = findViewById(R.id.txt2);
         btncalcular = findViewById(R.id.btncalcular);
+        btndividir = findViewById(R.id.btndividir);
+        btnmultiplicar = findViewById(R.id.btnmultiplicar);
+        btnrestar = findViewById(R.id.btnrestar);
         resultado = findViewById(R.id.resultado);
-        btncalcular.setOnClickListener(this);
+        btncalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int valor1 = Integer.parseInt(txt1.getText().toString());
+                int valor2 = Integer.parseInt(txt2.getText().toString());
+                float res = valor1 + valor2;
+                resultado.setText("La suma es: " + res);
+            }
+        });
+        btnrestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int valor1 = Integer.parseInt(txt1.getText().toString());
+                int valor2 = Integer.parseInt(txt2.getText().toString());
+                float res = valor1 - valor2;
+                resultado.setText("La resta es: " + res);
+            }
+        });
+        btnmultiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int valor1 = Integer.parseInt(txt1.getText().toString());
+                int valor2 = Integer.parseInt(txt2.getText().toString());
+                float res = valor1 * valor2;
+                resultado.setText("La multiplicación es: " + res);
+            }
+        });
+
+        btndividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int valor1 = Integer.parseInt(txt1.getText().toString());
+                int valor2 = Integer.parseInt(txt2.getText().toString());
+                float res = valor1 / valor2;
+                resultado.setText("La división es es: " + res);
+            }
+        });
+
     }
 
 
-    @Override
-    public void onClick(View v) {
-        int valor1 = Integer.parseInt(txt1.getText().toString());
-        int valor2 = Integer.parseInt(txt2.getText().toString());
-        int res = valor1 + valor2;
-        resultado.setText("La suma es: " + res);
-    }
+
 }
